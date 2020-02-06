b3bdeb6ca34481cea7c8ed228afc70e8a4ff32ed
Serge Modin <modinsv@yandex.ru>
2020-02-06 17:35:21 +0300

    Context added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from "react";
 import TodoList from "./Todo/TodoList";
+import Context from "./context";
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     { id: 1, completed: false, title: "Купить морковь" },
     { id: 2, completed: false, title: "Купить момлоко" },
     { id: 3, completed: false, title: "Купить машину" }
-  ];
+  ]);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
 
   return (
-    <div className='wrapper'>
-      <h1>React Tutorial</h1>
-      <TodoList todos={todos} />
-    </div>
+    <Context.Provider>
+      <div className='wrapper'>
+        <h1>React Tutorial</h1>
+        <TodoList todos={todos} onToggle={toggleTodo} />
+      </div>
+    </Context.Provider>
   );
 }
 
